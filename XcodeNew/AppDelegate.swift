a6fbfbb6e4106f405ad083fd0a4457ea03fdaa55
Farhan <mrfarhanzahoor@gmail.com>
2019-05-05 14:18:18 +0400

    thease area my new changes

--- XcodeNew/AppDelegate.swift
+++ XcodeNew/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
+        
+        print("New changess")
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
